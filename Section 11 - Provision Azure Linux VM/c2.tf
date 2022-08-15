a4aa042d86afb1e1506e0c6b2c9d4a8e71e1c7a8
bharadwaj55 <109013463+bharadwaj55@users.noreply.github.com>
2022-08-15 23:51:00 +0530

    Add files via upload

---
+++ Section 11 - Provision Azure Linux VM/c2.tf
@@ -0,0 +1,7 @@
+
+resource "azurerm_resource_group" "my-rg" {
+  name     = "my-rg1"
+  location = "East US"
+}
+
+
---
+++ Section 11 - Provision Azure Linux VM/c4.tf
@@ -0,0 +1,29 @@
+
+resource "azurerm_linux_virtual_machine" "mylinuxvm" {
+  name                = "mylinuxvm-1"
+  resource_group_name = azurerm_resource_group.my-rg.name
+  location            = azurerm_resource_group.my-rg.location
+  size                = "Standard_DS1_v2"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.myvmnic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("${path.module}/ssh-keys/terraform-azure.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "RedHat"
+    offer     = "RHEL"
+    sku       = "86-gen2"
+    version   = "latest"
+  }
+  ustom_data = filebase64("${path.module}/app-scripts/cloud-init-jenkins.txt")
+}
